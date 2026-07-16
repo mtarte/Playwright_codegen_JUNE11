79f3a67265638f331d83fb1e84d0d5572f820a11
mtarte <tartemr@gmail.com>
2026-07-15 21:11:22 -0700

    commit

--- tests/test_dropdown2.py
+++
@@ -1,8 +0,0 @@
-import re
-from playwright.sync_api import Page, expect
-
-
-def test_dropdown2(page: Page) -> None:
-    page.goto("https://the-internet.herokuapp.com/")
-    page.get_by_role("link", name="Dropdown").click()
-    page.locator("#dropdown").select_option("1")
