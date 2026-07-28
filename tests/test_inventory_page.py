53d6b9096cec0c40e3336e2255ab4fddddfa294b
mtarte <tartemr@gmail.com>
2026-07-27 19:00:01 -0700

    Commit

--- tests/test_inventory_page.py
+++ tests/test_inventory_page.py
@@ -4,35 +4,26 @@
     # Perform a Select from the Dropdown
     # assert the option was selected
 
-import re
-from playwright.sync_api import Page,expect
+import pytest
+from playwright.sync_api import Page
 
-from pages.InventoryPage import InventoryPage
 from pages.LoginPage import LoginPage
 
-
-def test_product_sort(page: Page) -> None:
-  
-    # login_page.open()
+@pytest.mark.parametrize(
+    "options",
+    [
+        ("az"),
+        ("za"),
+        ("lohi"),
+        ("hilo"),
+    ],
+)
+def test_sort_options(page: Page, options):
     login_page = LoginPage(page)
     login_page.open()
-    login_page.login_standard_user()
+    inventory_page = login_page.login_standard_user()
 
-    #Perform a Select from the Dropdown
+    inventory_page.sort_products_by(options)
 
-    page.get_by_text("Name (A to Z)Name (A to Z)").click()
-    page.get_by_text("Name (A to Z)Name (A to Z)").click()
-    page.locator("[data-test=\"product-sort-container\"]").select_option("za")
-    page.get_by_text("Name (Z to A)Name (A to Z)").click()
-    page.locator("[data-test=\"product-sort-container\"]").select_option("lohi")
-    page.get_by_text("Price (low to high)Name (A to").click()
-    page.locator("[data-test=\"product-sort-container\"]").select_option("hilo")
-    page.locator("[data-test=\"item-4-title-link\"]").click()
-    page.locator("[data-test=\"inventory-item-name\"]").click()
-    page.locator("[data-test=\"add-to-cart\"]").click()
-    page.get_by_text("Sauce Labs Backpackcarry.").click()
-    page.locator("[data-test=\"inventory-container\"] div").filter(has_text="Sauce Labs Backpackcarry.").first.click()
-   
-   #Assert the option was selected
-    assert page.get_by_text("Sauce Labs Backpack").text_content() == "Sauce Labs Backpack"
+    assert inventory_page.get_selected_sort() == options
     
